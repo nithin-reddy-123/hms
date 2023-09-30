c62a388e2f9ed663910ca9cf9cb2fb10bd6e20ad
Nithin <nithinreddy3210@gmail.com>
2023-10-01 00:25:02 +0530

    Implemented findhotelbyemail method, and overrided a method in userdetailsservice.

--- src/main/java/com/hms/service/implementation/HotelServiceImpl.java
+++ src/main/java/com/hms/service/implementation/HotelServiceImpl.java
@@ -1,8 +1,17 @@
 package com.hms.service.implementation;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.hms.entities.Role;
+import com.hms.entities.User;
+import com.hms.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.hms.entities.Counter;
@@ -15,6 +24,8 @@ public class HotelServiceImpl implements HotelService {
 	
 	@Autowired
 	private HotelRepository hotelRepository;
+	@Autowired
+	private UserRepository userRepository;
 
 	@Override
 	public List<Hotel> getAllHotels() {
@@ -67,5 +78,23 @@ public class HotelServiceImpl implements HotelService {
 		return hotelRepository.findHotelIdByHotelName(hotelName);
 	}
 
-	
+	@Override
+	public Hotel findHotelByEmail(String emailAddress) {
+		return hotelRepository.findHotelByEmail(emailAddress);
+	}
+
+
+	@Override
+	public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException {
+		Hotel hotel=hotelRepository.findHotelByEmail(emailAddress);
+		User user = userRepository.findByEmail(emailAddress);
+		if(hotel == null) {
+			throw new UsernameNotFoundException("Invalid username or password.");
+		}
+		return new org.springframework.security.core.userdetails.User(hotel.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+	}
+
+	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+	}
 }
