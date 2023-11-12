d7101954b4ce9a638fa40d985443aeb86a987106
Nithin <nithinreddy3210@gmail.com>
2023-11-12 12:30:15 +0530

    Removed unnecessary import statements

--- src/main/java/com/hms/HmsApplication.java
+++ src/main/java/com/hms/HmsApplication.java
@@ -2,10 +2,7 @@ package com.hms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 public class HmsApplication {
